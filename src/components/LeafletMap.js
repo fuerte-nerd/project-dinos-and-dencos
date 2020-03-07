3f3f50d195f98c4ae265a5de6bdf1f46148ac6c9
Fuertenerd <fuertenerd@gmail.com>
2020-03-07 16:22:08 +0000

    fixed leaflet rendering issue

--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -6,14 +6,8 @@ import { Icon } from "leaflet"
 import logoPin from "../images/logopin.png"
 
 export default function LeafletMap() {
-  const icon = new Icon({
-    iconUrl: logoPin,
-    iconSize: [40, 79],
-    iconAnchor: [20, 79],
-  })
   return typeof window !== "undefined" ? (
     <>
-     
       <Map
         center={[28.609654, -13.92936]}
         zoom={16}
@@ -27,7 +21,16 @@ export default function LeafletMap() {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
-        <Marker position={[28.609654, -13.92936]} icon={icon} />
+        <Marker
+          position={[28.609654, -13.92936]}
+          icon={
+            new Icon({
+              iconUrl: logoPin,
+              iconSize: [40, 79],
+              iconAnchor: [20, 79],
+            })
+          }
+        />
       </Map>
     </>
   ) : null
