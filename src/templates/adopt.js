962317d54a468ae0a20d443a1594788bf57cf6b3
Fuertenerd <fuertenerd@gmail.com>
2020-03-21 12:20:49 +0000

    Saved: 21-03-2020 12:20:49

--- src/templates/adopt.js
+++ src/templates/adopt.js
@@ -1,7 +1,12 @@
 import React from "react"
+import Layout from "../components/layout"
 
 const adopt = () => {
-  return <h1>I am the adopt page</h1>
+  return (
+    <Layout>
+      <h1>I am the adopt page</h1>
+    </Layout>
+  )
 }
 
 export default adopt
