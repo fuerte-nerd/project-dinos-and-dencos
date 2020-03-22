fd379f6a5407ce5fda331dc2528d0a8dd82223dd
Fuertenerd <fuertenerd@gmail.com>
2020-03-22 15:54:01 +0000

    Autogit: 22-03-2020 15:54 [skip ci]

---
+++ contact.md
@@ -0,0 +1,19 @@
+---
+background_image: assets/rafael-profile.jpg
+heading:
+  en: Get in touch!
+  es: ¡Ponerse en contacto!
+  de: In Kontakt kommen!
+  it: Mettiti in contatto!
+  fr: Entrer en contact!
+subheading:
+  en: >-
+    Elit sint libero consectetur dolor voluptatum! Molestias similique provident unde eaque totam? Voluptatibus eaque odit suscipit natus laboriosam qui fuga English?
+  es: >-
+    Lorem dolor odio voluptatum dolore veritatis! Accusantium ipsum tempore excepturi ratione commodi. Unde labore deserunt doloremque deserunt aliquid Laboriosam odio.
+  de: >-
+    Dolor eius laborum obcaecati placeat eius quos Exercitationem doloremque debitis nihil obcaecati dolores quae. Nemo ipsa at provident delectus ducimus!
+  it: >-
+    Adipisicing ipsum sequi eaque maiores impedit odit. Id minima id eum alias sint Esse quae quod optio explicabo nobis Quia?
+  fr: >-
+    Consectetur vero reprehenderit exercitationem voluptatum nostrum vitae Assumenda fugiat nostrum eveniet fugit iure. In sed doloremque illo atque veritatis Architecto
---
+++ contactTemplate.js
@@ -0,0 +1,83 @@
+import React from "react"
+import ReactMarkdown from "react-markdown"
+import { connect } from "react-redux"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import Link from "../components/Link"
+import BannerHead from "../components/BannerHead"
+import LangConsts from "../components/LanguageConstants"
+
+const staticPage = props => {
+  const { frontmatter } = props.data.text.childMarkdownRemark
+  console.log(props)
+
+  return (
+    <Layout
+      title={frontmatter.heading[props.lang]}
+      spacer={true}
+      showFooter={true}
+      og={{
+        description: frontmatter.subheading[props.lang],
+        image: "image+url+here",
+        url: "fuerteventuradogrescue.org",
+      }}
+    >
+      <BannerHead
+        heading={frontmatter.heading[props.lang]}
+        subheading={frontmatter.subheading[props.lang]}
+        path={props.path}
+      />
+      <div class="page-content extend-line-height container py-3">
+        <ReactMarkdown source={frontmatter.main[props.lang]} />
+        <div class="text-center">
+          <p class="mb-0 text-center">
+            {LangConsts.for_more_information[props.lang]}
+          </p>
+          <Link
+            to="/contact"
+            classes="btn btn-lg font-weight-bold btn-primary d-lg-inline-flex d-block align-items-center"
+          >
+            {LangConsts.contact_us[props.lang]}
+            <i class="ml-3 fas fa-paw"></i>
+          </Link>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+export const staticQuery = graphql`
+  query($id: String!) {
+    text: file(id: { eq: $id }) {
+      childMarkdownRemark {
+        frontmatter {
+          heading {
+            en
+            es
+            de
+            it
+            fr
+          }
+          main {
+            en
+            es
+            de
+            it
+            fr
+          }
+          subheading {
+            en
+            es
+            de
+            it
+            fr
+          }
+        }
+      }
+    }
+  }
+`
+
+const mapStateToProps = state => ({
+  lang: state.language.lang,
+})
+export default connect(mapStateToProps)(staticPage)
