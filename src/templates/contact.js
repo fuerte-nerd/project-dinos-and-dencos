fa39f5a4ac6921c4967a438de10a93aad0971d5f
Fuertenerd <fuertenerd@gmail.com>
2020-03-22 18:20:06 +0000

    Autogit: 22-03-2020 18:20 [skip ci]

--- src/templates/contact.js
+++ src/templates/contact.js
@@ -7,7 +7,6 @@ import LangConsts from "../components/LanguageConstants"
 
 const contactTemplate = props => {
   const { frontmatter } = props.data.text.childMarkdownRemark
-  console.log(props)
 
   return (
     <Layout
@@ -25,66 +24,69 @@ const contactTemplate = props => {
         subheading={frontmatter.subheading[props.lang]}
         path={props.path}
       />
-      <div class="page-content extend-line-height container py-3"></div>
-      <div class="my-3">
-        <h2>Messenger</h2>
-        <p class="text-left">
-          The most efficient way to contact us is via Facebook Messenger...
-        </p>
-        <a
-          href="http://www.messenger.com"
-          class="btn btn-lg font-weight-bold btn-primary d-block d-lg-inline-block align-items-center"
-        >
-          <div class="d-flex align-items-center justify-content-center">
-            Get in touch on Messenger{" "}
-            <i class="ml-3 fab fa-facebook-messenger display-4"></i>
-          </div>
-        </a>
-      </div>
-      <div class="my-3">
-        <h2>"I don't use Messenger..."</h2>
-        <p>
-          No problem! Complete and submit the form below and we will get back to
-          you as soon as possible...
-        </p>
-        <form>
-          <div class="row">
-            <div class="col-md-6">
-              <input
-                type="text"
-                class="form-control"
-                placeholder="First name"
-              />
+      <div class="page-content extend-line-height container py-3">
+        <div class="my-3">
+          <h2>Messenger</h2>
+          <p class="text-left">{frontmatter.messenger_intro[props.lang]}</p>
+          <a
+            href="http://www.messenger.com"
+            class="btn btn-lg font-weight-bold btn-primary d-block d-lg-inline-block align-items-center"
+          >
+            <div class="d-flex align-items-center justify-content-center">
+              Get in touch on Messenger{" "}
+              <i class="ml-3 fab fa-facebook-messenger display-4"></i>
             </div>
-            <div class="col-md-6 mt-3 mt-md-0">
-              <input type="text" class="form-control" placeholder="Last name" />
+          </a>
+        </div>
+        <div class="my-3">
+          <h2>"I don't use Messenger..."</h2>
+          <p>
+            No problem! Complete and submit the form below and we will get back
+            to you as soon as possible...
+          </p>
+          <form>
+            <div class="row">
+              <div class="col-md-6">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="First name"
+                />
+              </div>
+              <div class="col-md-6 mt-3 mt-md-0">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Last name"
+                />
+              </div>
+              <div class="col-md-6 mt-3">
+                <input type="email" class="form-control" placeholder="Email" />
+              </div>
+              <div class="col-md-6 mt-3">
+                <input type="text" class="form-control" placeholder="Phone" />
+              </div>
             </div>
-            <div class="col-md-6 mt-3">
-              <input type="email" class="form-control" placeholder="Email" />
+            <div class="row">
+              <div class="col mt-3">
+                <textarea
+                  class="form-control"
+                  id="exampleFormControlTextarea1"
+                  rows="3"
+                  placeholder="Write your message here..."
+                ></textarea>
+              </div>
             </div>
-            <div class="col-md-6 mt-3">
-              <input type="text" class="form-control" placeholder="Phone" />
+            <div class="text-center">
+              <button
+                type="submit"
+                class="btn btn-primary btn-lg btn-block font-weight-bold my-3"
+              >
+                Send <i class="ml-2 far fa-paper-plane"></i>
+              </button>
             </div>
-          </div>
-          <div class="row">
-            <div class="col mt-3">
-              <textarea
-                class="form-control"
-                id="exampleFormControlTextarea1"
-                rows="3"
-                placeholder="Write your message here..."
-              ></textarea>
-            </div>
-          </div>
-          <div class="text-center">
-            <button
-              type="submit"
-              class="btn btn-primary btn-lg btn-block font-weight-bold my-3"
-            >
-              Send <i class="ml-2 far fa-paper-plane"></i>
-            </button>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     </Layout>
   )
