d91331b15225c3c479f6a277063794fe9c5c8966
Fuerte Nerd <fuertenerd@gmail.com>
2020-03-25 12:36:07 +0000

    Autogit: 25-03-2020 12:36 [skip ci]

--- src/templates/thanks.js
+++ src/templates/thanks.js
@@ -16,21 +16,23 @@ export default function Thanks() {
       }}
     >
       <div className="min-vh-100 container d-flex justify-content-center align-items-center">
-        <img src={Logo} alt="FDR Logo" />
-        <p className="mt-3">
-          <span className="d-block font-weight-bold">
-            Thank you for your message!
-          </span>
-          We will reply as soon as possible.
-        </p>
+        <div className="message text-center animated fadeInDown">
+          <img src={Logo} alt="FDR Logo" />
+          <p className="mt-3">
+            <span className="d-block font-weight-bold">
+              Thank you for your message!
+            </span>
+            We will reply as soon as possible.
+          </p>
 
-        <p>
-          We will redirect you back to the site shortly, but if you can't
-          wait...
-        </p>
-        <Link to="contact" classes="btn btn-primary btn-lg ">
-          Take me back now!
-        </Link>
+          <p>
+            We will redirect you back to the site shortly, but if you can't
+            wait...
+          </p>
+          <Link to="contact" classes="btn btn-primary btn-lg ">
+            Take me back now!
+          </Link>
+        </div>
       </div>
     </Layout>
   )
