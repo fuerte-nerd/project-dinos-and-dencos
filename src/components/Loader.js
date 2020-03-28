64bfba3d89b419a2854066cb24f15902c7e194ff
Fuerte Nerd <fuertenerd@gmail.com>
2020-03-28 20:02:47 +0000

    Autogit: 28-03-2020 20:02 [skip ci]

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Loader = props => {
   return props.loaded ? (
-    <div className="min-vh-100 min-vw-100 bg-secondary"></div>
+    <div className="position-fixed min-vh-100 min-vw-100 bg-secondary"></div>
   ) : null
 }
 
